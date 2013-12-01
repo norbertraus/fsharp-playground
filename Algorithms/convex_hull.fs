2efb82164f20fe04620b389a3bf163d0dad5e84d
Norbert Raus <norbert.raus@gmail.com>
2013-12-01 18:49:21 +0000

    additional changes

--- Algorithms/convex_hull.fs
+++ Algorithms/convex_hull.fs
@@ -14,7 +14,7 @@ type Turn =
   | Right
 
 [<System.Diagnostics.DebuggerDisplay("x={X}, y={Y}")>]
-type public Point2D = { X:double; Y:double }
+type Point2D = { X:double; Y:double }
 
 //translates from radians to degrees between 0-360
 let degree r = 
@@ -70,6 +70,20 @@ let polar_sort (points:Point2D list) =
   let s = List.tail sorted |> List.sortWith(fun p1 p2 -> cmp p1 p2) 
   fst :: s
    
+let polar_sort2 (points:Point2D list) =
+  //choose extreme point with the loweset y and x
+  let sorted = points |> List.sortWith (fun p1 p2 -> 
+    match p1.Y.CompareTo(p2.Y) with
+    | 0 -> p1.X.CompareTo(p2.X)
+    | _ as c -> c) 
+
+  let refPoint, tail = sorted.Head, sorted.Tail
+  
+  refPoint :: (tail |> List.sortBy (fun p -> 
+    let dx = p.X - refPoint.X
+    let dy = p.Y - refPoint.Y
+    Math.Atan2(dy, dx)))
+
 let hull (points: Point2D list) =
   let rec hull' p s =
     match p with
@@ -78,13 +92,91 @@ let hull (points: Point2D list) =
       match s with
       | a::b::ss -> 
         //match turn b a x with
-        match turn2 b a x with
+        match turn b a x with
         | Left -> hull' xs (x::s)
         | Right -> hull' p (b::ss)
       | _ -> hull' xs (x::s)
 
-  let sorted = polar_sort points
+  let sorted = polar_sort2 points
   hull' sorted []
+
+module GrahamScan =
+
+  type Point = { X : float; Y : float }
+
+  /// finds the points lying on the convex hull of the given set of points and 
+  /// returns those points in clockwise direction, starting at the point
+  /// with minimum y-value
+  /// Remarks: it's a more or less direct implementation of the algorithm named
+  /// after Ronald Graham that is explained on http://en.wikipedia.org/wiki/Graham_scan
+  let FindConvexHull (pts : Point seq) : Point seq =
+
+      // it's nicer to work with lists so let's convert
+      let ptl = List.ofSeq pts
+
+      // to make something worthwhile we need at last two points
+      if ptl.Length <= 2 then Seq.empty
+      else
+
+      // this is a helperfunction (explained in the wikipedia article) in which direction
+      // 3 points "turn"
+      let ccw (a : Point) (b : Point) (c : Point) =
+          (b.X - a.X)*(c.Y - a.Y) - (b.Y - a.Y)*(c.X - a.X)
+
+      // 1. Let's find the point with the minimum y-coordinate
+      /// this is the comparision function for this
+      let cmpPts (a : Point) (b : Point) =
+          match a.Y.CompareTo(b.Y) with
+          | 0 -> a.X.CompareTo(b.X)
+          | _ as r -> r
+      // and with it we can look for the mentioned point
+      let sortedY = ptl |> List.sortWith cmpPts
+      let org, rest = sortedY.Head, sortedY.Tail
+
+      // 2. we have to sort the list in increasing order of the angle
+      // that a point p makes with org and the x-axis
+      let winkelCos (p : Point) =
+          let dx = float (p.X - org.X)
+          let dy = float (p.Y - org.Y)
+          let l = System.Math.Sqrt(dx*dx + dy*dy)
+          dx / l
+      // and here we sort the list (we only sort the remainder without
+      // org and prepend it afterwards to ward of 
+      // any issue with "division by zero"
+      let sortedW = org::(rest |> List.sortBy winkelCos)
+
+      // here is the actual algorithm
+      // it uses two lists
+      // lastPts: every visited point is put but might
+      //          be removed if the "turn direction"
+      //          'turns' out to be wrong
+      // nextPts: the points left to be checked
+      //          as the algorithm progresses those
+      //          points are moved to lastPts
+      // so lastPts will contain the found points
+      // on the convex hull at every step, but in
+      // clockwise orientation (as we push in front
+      // of the list)
+      let rec scan (lastPts : Point list) (nextPts : Point list) =
+          // we are done if there are no points left to check
+          if nextPts.IsEmpty then lastPts
+          else
+
+          // if there are points left take the first one
+          let c = nextPts.Head
+
+          match lastPts with
+          // if there are at least 2 points b,a in the visited points
+          // and a,b,c is NOT a counterclockwise turn
+          // we have to remove b from lastPoints and continue checking
+          // backwards ...
+          | b::a::_ when ccw a b c >= 0. -> scan (lastPts.Tail) nextPts
+          // in every other case we can push c onto the visited
+          // stack and continue
+          | _ -> scan (c::lastPts) nextPts.Tail
+
+      // run it
+      sortedW |> scan [] |> Seq.ofList
 
 module UI =
   
@@ -126,7 +218,16 @@ module UI =
     Controls.Canvas.SetBottom(ellipse, 500.0 - p.Y - size / 2.0)
     poly.Points.Clear()
 
-    points |> Array.toList |> hull |> List.iter (fun p -> poly.Points.Add(point(p)))
+    points 
+    |> Array.toList 
+    |> hull 
+    |> List.iter (fun p -> poly.Points.Add(point(p)))
+
+//    let pp:GrahamScan.Point[]  = points |> Array.map (fun p -> {X=p.X;Y=p.Y})
+//    pp 
+//    |> GrahamScan.FindConvexHull 
+//    |> Seq.toList 
+//    |> List.iter (fun p -> poly.Points.Add(Windows.Point(p.X, p.Y)))
 
   [<System.STAThread>]
   let run =
@@ -149,8 +250,10 @@ module testing =
   [<Test>]
   let should_return_convex_hull() =
     let points = [for x,y in [1.0,1.0; 3.0,3.0; 4.0,6.0; 10.0,8.0; 9.0,14.0; 7.0,17.0; 5.0,15.0; 4.0,12.0; 2.0,13.0; 3.0,9.0] -> {X=x;Y=y}]
-    let convex = List.rev(hull points) 
-    Assert.That(convex, Is.EqualTo([for x,y in [1.0,1.0; 10.0,8.0; 9.0,14.0; 7.0,17.0; 2.0,13.0] -> {X=x;Y=y}]))
+    let pp:GrahamScan.Point list  = points |> List.map (fun p -> {X=p.X;Y=p.Y})
+    let convex = GrahamScan.FindConvexHull pp |> Seq.map (fun p -> {X=p.X;Y=p.Y}) |> Seq.toList |> List.rev
+    let convex2 = hull points |> List.rev 
+    Assert.That(convex2, Is.EqualTo([for x,y in [1.0,1.0; 10.0,8.0; 9.0,14.0; 7.0,17.0; 2.0,13.0] -> {X=x;Y=y}]))
 
   [<Test>]
   let should_sort_by_polar_angle() =
